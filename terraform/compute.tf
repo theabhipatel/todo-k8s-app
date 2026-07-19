ae63910c3d47dbeb6eb0c1bdc9fd0ea0e9e34f08
theabhipatel <theabhipatel.co@gmail.com>
2026-07-19 14:15:27 +0530

    chore: update tf files

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -60,6 +60,11 @@ resource "aws_instance" "main" {
   vpc_security_group_ids      = [aws_security_group.main.id]
   associate_public_ip_address = true
 
+   root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+  }
+
   tags = {
     Name = "todo-server"
   }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,4 +1,4 @@
 aws_region    = "us-east-1"
-instance_type = "t3.large"
+instance_type = "t3.xlarge"
 key_name      = "usEast-1-key"
 my_ip         = "0.0.0.0/0"
