5d10024caba1c0979321aed83b48718cad336a5c
theabhipatel <theabhipatel.co@gmail.com>
2026-07-26 17:07:37 +0530

    chore: add domain for the grafana using terraform

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -15,3 +15,12 @@ resource "cloudflare_dns_record" "todo_api" {
   ttl     = 1
   proxied = false
 }
+
+resource "cloudflare_dns_record" "grafana" {
+  zone_id = var.cloudflare_zone_id
+  name    = "grafana.todo.theabhipatel.com"
+  type    = "A"
+  content = aws_eip.main.public_ip
+  ttl     = 1
+  proxied = false
+}
